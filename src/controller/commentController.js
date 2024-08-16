a54991597173032d7a36f4ba850b821de5367f79
Kihwan <eljsh95@naver.com>
2024-08-16 13:14:44 +0900

    책 평가 , 지난 이야기 조회

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var commentService = require('../service/commentService')
+var commentService = require('../service/commentService');
 
-router.post("/create", async function(req, res, next) {
+router.post('/create', async function (req, res, next) {
     res.json(await commentService.commentCreate(req, res, next));
-})
+});
 
-router.post("/update", async function(req, res, next) {
-    res.json(await commentService.commentUpdate(req,res,next))
-})
+router.post('/update', async function (req, res, next) {
+    res.json(await commentService.commentUpdate(req, res, next));
+});
 
-module.exports = router
+module.exports = router;
--- src/service/bookService.js
+++ src/service/bookService.js
@@ -1,42 +1,41 @@
-const pool = require('../common/config/db')
-const mapper = require("mybatis-mapper")
-
+const pool = require('../common/config/db');
+const mapper = require('mybatis-mapper');
 
 module.exports.monthlyBookCreate = async function monthlyBookCreate(req, res, next) {
     var memberIdx = 0;
 
-    if(req.cookies['member']) {
+    if (req.cookies['member']) {
         memberIdx = req.cookies['member'];
     } else {
         return {
-            code : -1,
-            message : "fail"
+            code: -1,
+            message: 'fail',
         };
     }
-    console.log(memberIdx)
+    console.log(memberIdx);
 
     // 책 작성 가능 멤버
     var result;
-    try{
-        var query = mapper.getStatement("query", "isSelectedDate", {memberIdx: memberIdx})
+    try {
+        var query = mapper.getStatement('query', 'isSelectedDate', { memberIdx: memberIdx });
         result = await pool.query(query);
     } catch (e) {
-        console.log("isSelectedDate error - ", e.message);
+        console.log('isSelectedDate error - ', e.message);
 
         return {
-            "code" : -3,
-            "message" : "fail"
-        }
+            code: -3,
+            message: 'fail',
+        };
     }
 
     var resultDate = new Date(result);
     var canCreateDate = new Date();
-    canCreateDate = canCreateDate.setMonth((canCreateDate.setMonth() - 3))
+    canCreateDate = canCreateDate.setMonth(canCreateDate.setMonth() - 3);
 
-    if(resultDate.getMonth >= canCreateDate.getMonth) {
+    if (resultDate.getMonth >= canCreateDate.getMonth) {
         return {
-            code : -2,
-            message : "fail"
+            code: -2,
+            message: 'fail',
         };
     }
 
@@ -51,22 +50,22 @@ module.exports.monthlyBookCreate = async function monthlyBookCreate(req, res, ne
     var param = req.body;
     var result;
     try {
-        var query = mapper.getStatement("query", "selectBookType", param)
+        var query = mapper.getStatement('query', 'selectBookType', param);
         result = await pool.query(query);
     } catch (e) {
-        console.log("selectBookType error - ", e.message);
+        console.log('selectBookType error - ', e.message);
 
         return {
-            "code" : -3,
-            "message" : "fail"
-        }
+            code: -3,
+            message: 'fail',
+        };
     }
 
     var bookType = 1;
 
-    if(result[0].length > 0) {
-        for await(var resultType of result[0]) {
-            if(resultType.bookType === 3) {
+    if (result[0].length > 0) {
+        for await (var resultType of result[0]) {
+            if (resultType.bookType === 3) {
                 bookType = 3;
                 break;
             }
@@ -76,361 +75,346 @@ module.exports.monthlyBookCreate = async function monthlyBookCreate(req, res, ne
 
     var result;
     try {
-        var query = mapper.getStatement("query", "createMonthlyBook", 
-            {...param, memberIdx:memberIdx, bookType: bookType, imgUrl:req.file.originalname})
+        var query = mapper.getStatement('query', 'createMonthlyBook', { ...param, memberIdx: memberIdx, bookType: bookType, imgUrl: req.file.originalname });
         result = await pool.query(query);
     } catch (e) {
-        console.log("createMonthlyBook error - ", e.message)
+        console.log('createMonthlyBook error - ', e.message);
         return {
             code: -3,
-            message: "fail"
+            message: 'fail',
         };
     }
 
-    if(result[0].affectedRows > 0) {
+    if (result[0].affectedRows > 0) {
         return {
             code: 1,
-            message: "success"
+            message: 'success',
         };
     }
 
     return {
         code: -3,
-        message: "fail"
+        message: 'fail',
     };
-
-}
+};
 
 module.exports.monthlyBookList = async function monthlyBookList(res, req, next) {
     var result;
     try {
-        var query = mapper.getStatement("query", "selectMonthlyRecommendedBook")
-        result = await pool.query(query)
-    } catch(e) {
-        console.log("selectMonthlyRecommendedBook error - ", e.message)
+        var query = mapper.getStatement('query', 'selectMonthlyRecommendedBook');
+        result = await pool.query(query);
+    } catch (e) {
+        console.log('selectMonthlyRecommendedBook error - ', e.message);
         return {
             code: -2,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
 
-    if(result[0].length === 0) {
+    if (result[0].length === 0) {
         return {
             code: -1,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
 
     return {
         code: 1,
-        meesage: "success",
-        data: result[0]
-    }
-
-}
+        meesage: 'success',
+        data: result[0],
+    };
+};
 
 module.exports.monthlyBook = async function monthlyBook(req, res, next) {
     var bookIdx = req.params.bookIdx;
 
     var result;
     try {
-        var query = mapper.getStatement("query", "selectMonthlyBook", {bookIdx: bookIdx});
+        var query = mapper.getStatement('query', 'selectMonthlyBook', { bookIdx: bookIdx });
         result = await pool.query(query);
     } catch (e) {
-        console.log("selectMonthlyBook error - ", e.message)
+        console.log('selectMonthlyBook error - ', e.message);
         return {
             code: -2,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
 
-    if(result[0].length < 1) {
+    if (result[0].length < 1) {
         return {
             code: -1,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
 
     var result;
     try {
-        var query = mapper.getStatement("query", "selectCommentListByBookIdx", {bookIdx: bookIdx})
+        var query = mapper.getStatement('query', 'selectCommentListByBookIdx', { bookIdx: bookIdx });
         resultCommentList = await pool.query(query);
     } catch (e) {
-        console.log("selectCommentListByBookIdx error - ", e.message)
+        console.log('selectCommentListByBookIdx error - ', e.message);
         return {
             code: -2,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
 
     var resultJson = {};
     resultJson = result[0];
-    resultJson = {...resultJson, commentData:resultCommentList[0]}
+    resultJson = { ...resultJson, commentData: resultCommentList[0] };
 
     return {
         code: 1,
-        message: "success",
-        data: resultJson
-    }
-
-}
+        message: 'success',
+        data: resultJson,
+    };
+};
 
 module.exports.monthlyBookElect = async function monthlyBookElect(req, res, next) {
     var electedBook = req.body.bookIdx;
 
     // 이전에 당선됐던 책 (type 2) type 3으로 변경
     try {
-        var query = mapper.getStatement("query", "updateBeforeElectMonthlyBook")
+        var query = mapper.getStatement('query', 'updateBeforeElectMonthlyBook');
         var result = await pool.query(query);
 
-        if(result[0].affectedRows !== 1) {
+        if (result[0].affectedRows !== 1) {
             return {
                 code: -1,
-                messaget: "fail"
-            }
+                messaget: 'fail',
+            };
         }
-
     } catch (e) {
-        console.log("updateBeforeElectMonthlyBook error - ", e.message)
+        console.log('updateBeforeElectMonthlyBook error - ', e.message);
         return {
             code: -99,
-            message: "fail"
+            message: 'fail',
         };
     }
 
     // 당선 된 책 type 2 으로 변경
     try {
-        var query = mapper.getStatement("query", "updateElectMonthlyBook", {bookIdx: electedBook})
+        var query = mapper.getStatement('query', 'updateElectMonthlyBook', { bookIdx: electedBook });
         var result = await pool.query(query);
 
-        if(result[0].affectedRows !== 1) {
+        if (result[0].affectedRows !== 1) {
             return {
                 code: -2,
-                messaget: "fail"
-            }
+                messaget: 'fail',
+            };
         }
     } catch (e) {
-        console.log("updateElectMonthlyBook error - ", e.message)
+        console.log('updateElectMonthlyBook error - ', e.message);
         return {
             code: -99,
-            message: "fail"
+            message: 'fail',
         };
     }
 
     // 다른 추천 책 모두 type 4 로 변경
     try {
-        var query = mapper.getStatement("query", "updateRecommendBookType")
+        var query = mapper.getStatement('query', 'updateRecommendBookType');
         var result = await pool.query(query);
 
-        if(result[0].affectedRows < 1) {
+        if (result[0].affectedRows < 1) {
             return {
                 code: -3,
-                messaget: "fail"
-            }
+                messaget: 'fail',
+            };
         }
     } catch (e) {
-        console.log("updateRecommendBookType error - ", e.message)
+        console.log('updateRecommendBookType error - ', e.message);
         return {
             code: -99,
-            message: "fail"
+            message: 'fail',
         };
     }
 
     // 당선이 끝나면 추천인 last_selected_date 업데이트
     try {
-        var query = mapper.getStatement("query", "selectAndUpdateMemberIdxByBookIdx", {bookIdx:electedBook})
+        var query = mapper.getStatement('query', 'selectAndUpdateMemberIdxByBookIdx', { bookIdx: electedBook });
         var result = await pool.query(query);
 
-        if(result[0].affectedRows < 1) {
+        if (result[0].affectedRows < 1) {
             return {
                 code: -4,
-                messaget: "fail"
-            }
+                messaget: 'fail',
+            };
         }
     } catch (e) {
-        console.log("selectAndUpdateMemberIdxByBookIdx error - ", e.message)
+        console.log('selectAndUpdateMemberIdxByBookIdx error - ', e.message);
         return {
             code: -99,
-            message: "fail"
+            message: 'fail',
         };
     }
 
     return {
         code: 1,
-        message: "success"
-    }
-
-}
+        message: 'success',
+    };
+};
 
 module.exports.monthlyBookEvaluate = async function monthlyBookEvaluate(req, res, next) {
     var bookIdx = req.params.bookIdx;
     var memberIdx = 0;
-    if(req.cookies['member']) {
+    if (req.cookies['member']) {
         memberIdx = req.cookies['member'];
     } else {
         return {
-            code : -1,
-            message : "fail"
+            code: -1,
+            message: 'fail',
         };
     }
 
     // 책이 당선된 책이 아닌 경우에 대한 검사
     try {
-        var query = mapper.getStatement("query", "selectBookTypeByIdx", {bookIdx: bookIdx})
+        var query = mapper.getStatement('query', 'selectBookTypeByIdx', { bookIdx: bookIdx });
         var result = await pool.query(query);
 
-        if(result[0][0].bookType !== 2) {
+        if (result[0][0].bookType !== 2) {
             return {
                 code: -2,
-                message: "fail"
-            }
+                message: 'fail',
+            };
         }
     } catch (e) {
-        console.log("selectBookTypeByIdx error - ", e.message);
+        console.log('selectBookTypeByIdx error - ', e.message);
         return {
             code: -99,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
-
-
 
     /**
      * contents, star
      */
-    var reqBody = req.body
+    var reqBody = req.body.reviewData;
     try {
-        var query = mapper.getStatement("query", "insertBookEvaluate", {...reqBody, bookIdx:bookIdx, memberIdx:memberIdx})
+        var query = mapper.getStatement('query', 'insertBookEvaluate', { ...reqBody, bookIdx: bookIdx, memberIdx: memberIdx });
         var result = await pool.query(query);
-    
-        if(result[0].affectedRows > 0) {
+
+        if (result[0].affectedRows > 0) {
             return {
                 code: 1,
-                message: "success"
-            }
+                message: 'success',
+            };
         }
-    } catch(e) {
-        console.log("insertBookEvaluate error - ", e.message)
+    } catch (e) {
+        console.log('insertBookEvaluate error - ', e.message);
         return {
             code: -99,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
-
 
     return {
         code: -1,
-        message: "fail"
-    }
-}
+        message: 'fail',
+    };
+};
 
 // book type 3 : 이미 선정됬었던 도서 검색
 module.exports.lastBookList = async function lastBookList(req, res, next) {
     var year = req.query.year;
-    if( year === undefined || year === null) {
+    if (year === undefined || year === null) {
         return {
             code: -2,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
 
     try {
-        var query = mapper.getStatement("query", "selectLastBookList", {year: year})
+        var query = mapper.getStatement('query', 'selectLastBookList', { year: year });
         var result = await pool.query(query);
-    
-        if(result[0].length < 1) {
+
+        if (result[0].length < 1) {
             return {
                 code: -1,
-                message: "none"
-            }
+                message: 'none',
+            };
         }
-        
+
         return {
             code: 1,
-            message: "success",
-            data: result[0]
+            message: 'success',
+            data: result[0],
         };
-
     } catch (e) {
-        console.log("selectLastBookList error - ", e.message)
+        console.log('selectLastBookList error - ', e.message);
 
         return {
             code: -99,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
-
-
-}
+};
 
 /**
- * 
- * @param {*} req 
+ *
+ * @param {*} req
  * @param {*} res : 책 idx, 책 이미지, 제목, 출판사 , 카테고리 , 추천인, 추천 이유
- * @param {*} next 
+ * @param {*} next
  */
 module.exports.getThisMonthElectedBook = async function getThisMonthElectedBook(req, res, next) {
-
     // 이번 달
     var nowDate = new Date();
     var year = nowDate.getFullYear();
     month = nowDate.getMonth() + 1;
 
-    var targetDate = year +
-                '-' + ( month < 9 ? "0" + month : month );
+    var targetDate = year + '-' + (month < 9 ? '0' + month : month);
 
     try {
-        var query = mapper.getStatement("query", "selectThisMonthlyElectedBook", {targetDate: targetDate})
+        var query = mapper.getStatement('query', 'selectThisMonthlyElectedBook', { targetDate: targetDate });
         var result = await pool.query(query);
 
         console.log(query);
 
-        if(result[0].length > 0) {
+        if (result[0].length > 0) {
             return {
                 code: 1,
-                message: "success",
-                data: result[0][0]
-            }
-        }
-        else {
+                message: 'success',
+                data: result[0][0],
+            };
+        } else {
             return {
                 code: -1,
-                message: "fail"
-            }
+                message: 'fail',
+            };
         }
     } catch (e) {
-        console.log("selectThisMonthlyElectedBook error - ", e.message);
+        console.log('selectThisMonthlyElectedBook error - ', e.message);
         return {
             code: -99,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
-}
+};
 
 module.exports.monthlyBookEvaluateList = async function monthlyBookEvaluateList(req, res, next) {
     const bookIdx = req.params.bookIdx;
 
     try {
-        var query = mapper.getStatement("query", "selectBookEvaluateList", {bookIdx: bookIdx});
+        var query = mapper.getStatement('query', 'selectBookEvaluateList', { bookIdx: bookIdx });
         var result = await pool.query(query);
 
-        if(result[0].length > 0) {
+        if (result[0].length > 0) {
             return {
-                code : 1,
-                message : "success",
-                data: result[0]
-            }
+                code: 1,
+                message: 'success',
+                data: result[0],
+            };
         } else {
             return {
                 code: -1,
-                message: "fail"
-            }
+                message: 'fail',
+            };
         }
     } catch (e) {
-        console.log("selectBookEvaluateList error - ", e.message);
+        console.log('selectBookEvaluateList error - ', e.message);
         return {
             code: -99,
-            message: "fail"
-        }
+            message: 'fail',
+        };
     }
-}
+};
